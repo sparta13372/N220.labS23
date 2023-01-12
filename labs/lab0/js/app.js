dad95f9eedc2fa8e2723cb9548326bd425c511b5
sparta13372 <121908207+sparta13372@users.noreply.github.com>
2023-01-12 11:40:36 -0500

    Creating Lab 0

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,15 @@
+function setup () {
+    createCanvas(1200,768);
+
+    background( 135, 64, 207 );
+
+    fill(199,169,72)
+    rect( 40,50,100,20)
+
+    
+}
+
+function draw() {
+    circle(mouseX, mouseY, 25)
+}
+
